ca6e4faaf5b5860a65ebc1aea6c90b92db6d1028
Lucas Souza <38145232+lss8@users.noreply.github.com>
2021-11-26 11:23:37 -0300

    Task 02 - RPNstacker
    Added Token and TokenTyper files, and updatede RPNstacker file.

--- compilers/RPNstacker.java
+++ compilers/RPNstacker.java
@@ -2,38 +2,55 @@ import java.util.*;
 import java.io.*;
 
 public class RPNstacker { 
-    public static void main(String[] args) throws FileNotFoundException {     
+    public static void main(String[] args) throws Exception {     
         File RPN = new File("/home/lucas/Documents/compilers/Calc1.stk");
         Scanner in = new Scanner(RPN); 
         Stack<Integer> pile = new Stack<Integer>();
         int result=0;
         
+        List<Token> tokens=new ArrayList<Token>();  
+
         while (in.hasNext()) {
                 String line = in.nextLine();
-        
+                
                 if (isNumeric(line)) {
+                    tokens.add(new Token(TokenType.NUM,line));
                     int num = Integer.parseInt(line) ;
                     pile.push(num);
                 }
                 else {
-                    int num2 = pile.pop();
-                    int num1 = pile.pop();
                     if (line.equals("+")) {
+                        tokens.add(new Token(TokenType.PLUS,line));
+                        int num2 = pile.pop();
+                        int num1 = pile.pop();
                         result = num1+num2;
                     }
-                    if (line.equals("-")) {
+                    else if (line.equals("-")) {
+                        tokens.add(new Token(TokenType.MINUS,line));
+                        int num2 = pile.pop();
+                        int num1 = pile.pop();
                         result = num1-num2;
                     }
-                    if (line.equals("*")) {
+                    else if (line.equals("*")) {
+                        tokens.add(new Token(TokenType.STAR,line));
+                        int num2 = pile.pop();
+                        int num1 = pile.pop();
                         result = num1*num2;
                     }
-                    if (line.equals("/")) {
+                    else if (line.equals("/")) {
+                        tokens.add(new Token(TokenType.SLASH,line));
+                        int num2 = pile.pop();
+                        int num1 = pile.pop();
                         result = num1/num2;
+                    }
+                    else {
+                        throw new Exception("Unexpected character: " + line); 
                     }
                     pile.push(result);
                 }
         }
         System.out.println(pile.pop());
+        System.out.println(tokens);
     } 
 
     private static boolean isNumeric(String str){
